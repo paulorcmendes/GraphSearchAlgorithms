750bbf619fb0b991a100dd41b101061b6cb1bdc9
paulorcmendes <mendesdpn@gmail.com>
2017-10-06 00:08:26 -0300

    Edges set up

--- GraphSearchAlgorithms/Program.cs
+++ GraphSearchAlgorithms/Program.cs
@@ -24,14 +24,84 @@ namespace GraphSearchAlgorithms
             InitEdges();
         }
         static void InitEdges() {
+            graph["Arad"].Neighbors.Add(new Neighbor(graph["Sibiu"], 140));
+            graph["Arad"].Neighbors.Add(new Neighbor(graph["Timisoara"], 118));
+            graph["Arad"].Neighbors.Add(new Neighbor(graph["Zerind"], 75));
 
+            graph["Zerind"].Neighbors.Add(new Neighbor(graph["Arad"], 75));
+            graph["Zerind"].Neighbors.Add(new Neighbor(graph["Oradea"], 71));
+
+            graph["Oradea"].Neighbors.Add(new Neighbor(graph["Sibiu"], 151));
+            graph["Oradea"].Neighbors.Add(new Neighbor(graph["Zerind"], 71));
+
+            graph["Timisoara"].Neighbors.Add(new Neighbor(graph["Arad"], 118));
+            graph["Timisoara"].Neighbors.Add(new Neighbor(graph["Lugoj"], 111));
+
+            graph["Lugoj"].Neighbors.Add(new Neighbor(graph["Mehadia"], 70));
+            graph["Lugoj"].Neighbors.Add(new Neighbor(graph["Timisoara"], 111));
+
+            graph["Mehadia"].Neighbors.Add(new Neighbor(graph["Drobeta"], 75));
+            graph["Mehadia"].Neighbors.Add(new Neighbor(graph["Lugoj"], 70));
+
+            graph["Drobeta"].Neighbors.Add(new Neighbor(graph["Craiova"], 120));
+            graph["Drobeta"].Neighbors.Add(new Neighbor(graph["Mehadia"], 75));
+
+            graph["Craiova"].Neighbors.Add(new Neighbor(graph["Drobeta"], 120));
+            graph["Craiova"].Neighbors.Add(new Neighbor(graph["Pitesti"], 138));
+            graph["Craiova"].Neighbors.Add(new Neighbor(graph["Rimnicu"], 146));
+
+            graph["Sibiu"].Neighbors.Add(new Neighbor(graph["Arad"], 140));
+            graph["Sibiu"].Neighbors.Add(new Neighbor(graph["Fagaras"], 99));
+            graph["Sibiu"].Neighbors.Add(new Neighbor(graph["Oradea"], 151));
+            graph["Sibiu"].Neighbors.Add(new Neighbor(graph["Rimnicu"], 80));
+
+            graph["Fagaras"].Neighbors.Add(new Neighbor(graph["Bucareste"], 211));
+            graph["Fagaras"].Neighbors.Add(new Neighbor(graph["Sibiu"], 99));
+
+            graph["Rimnicu"].Neighbors.Add(new Neighbor(graph["Craiova"], 146));
+            graph["Rimnicu"].Neighbors.Add(new Neighbor(graph["Pitesti"], 97));
+            graph["Rimnicu"].Neighbors.Add(new Neighbor(graph["Sibiu"], 80));
+
+            graph["Pitesti"].Neighbors.Add(new Neighbor(graph["Bucareste"], 101));
+            graph["Pitesti"].Neighbors.Add(new Neighbor(graph["Craiova"], 138));
+            graph["Pitesti"].Neighbors.Add(new Neighbor(graph["Rimnicu"], 97));
+
+            graph["Neamt"].Neighbors.Add(new Neighbor(graph["Iasi"], 87));
+
+            graph["Iasi"].Neighbors.Add(new Neighbor(graph["Neamt"], 87));
+            graph["Iasi"].Neighbors.Add(new Neighbor(graph["Vaslui"], 92));
+
+            graph["Vaslui"].Neighbors.Add(new Neighbor(graph["Iasi"], 92));
+            graph["Vaslui"].Neighbors.Add(new Neighbor(graph["Urziceni"], 142));
+
+            graph["Bucareste"].Neighbors.Add(new Neighbor(graph["Fagaras"], 211));
+            graph["Bucareste"].Neighbors.Add(new Neighbor(graph["Giurgiu"], 90));
+            graph["Bucareste"].Neighbors.Add(new Neighbor(graph["Pitesti"], 101));
+            graph["Bucareste"].Neighbors.Add(new Neighbor(graph["Urziceni"], 85));
+
+            graph["Giurgiu"].Neighbors.Add(new Neighbor(graph["Bucareste"], 90));
+
+            graph["Urziceni"].Neighbors.Add(new Neighbor(graph["Bucareste"], 85));
+            graph["Urziceni"].Neighbors.Add(new Neighbor(graph["Hirsova"], 98));
+            graph["Urziceni"].Neighbors.Add(new Neighbor(graph["Vaslui"], 142));
+
+            graph["Hirsova"].Neighbors.Add(new Neighbor(graph["Eforie"], 86));
+            graph["Hirsova"].Neighbors.Add(new Neighbor(graph["Urziceni"], 98));
+
+            graph["Eforie"].Neighbors.Add(new Neighbor(graph["Hirsova"], 86));
         }
         static void Main(string[] args)
         {
             InitGraph();
             foreach(KeyValuePair<String, Node> entry in graph)
             {
-                Console.WriteLine(entry.Value.Name);
+                Console.Write(entry.Value.Name+": ");
+                foreach (Neighbor neighbor in entry.Value.Neighbors)
+                {
+                    Console.Write(neighbor.Cost+", ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
             }
 
             Console.ReadKey();
