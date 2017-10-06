541028827f57e443859aba4cf09b7548481a423b
paulorcmendes <mendesdpn@gmail.com>
2017-10-05 23:25:11 -0300

    Modified nodes to graph

--- GraphSearchAlgorithms/Program.cs
+++ GraphSearchAlgorithms/Program.cs
@@ -9,23 +9,27 @@ namespace GraphSearchAlgorithms
 {
     class Program
     {
-        static Dictionary<String, Node> nodes;
+        static Dictionary<String, Node> graph;
         static void InitGraph() {
             String[] namesOfNodes = { "Arad", "Zerind", "Oradea", "Timisoara",
                                       "Lugoj", "Mehadia", "Drobeta", "Craiova",
                                       "Sibiu", "Fagaras", "Rimnicu", "Pitesti",
                                       "Neamt", "Iasi", "Vaslui", "Bucareste",
                                       "Giurgiu", "Urziceni", "Hirsova", "Eforie"};
-            nodes = new Dictionary<String, Node>();
-            foreach(String name in namesOfNodes)
+            graph = new Dictionary<String, Node>();
+            foreach (String name in namesOfNodes)
             {
-                nodes.Add(name, new Node(name));
+                graph.Add(name, new Node(name));
             }
+            InitEdges();
+        }
+        static void InitEdges() {
+
         }
         static void Main(string[] args)
         {
             InitGraph();
-            foreach(KeyValuePair<String, Node> entry in nodes)
+            foreach(KeyValuePair<String, Node> entry in graph)
             {
                 Console.WriteLine(entry.Value.Name);
             }
